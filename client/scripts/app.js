b725f51a22bf849ca09ebc90d7f39b1bb60ac61d
jdbyrd <jd3yrd@gmail.com>
2017-12-01 15:13:20 -0600

    Pass all tests through fetching

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,43 @@
-// YOUR CODE HERE:
+// const message = {
+//   username: 'spencer&james!',
+//   text: 'we\'re awesome',
+//   roomname: ''
+// };
+
+const app = {
+  
+  init: function() {
+    
+  },
+  send: function(message) {
+    $.ajax({
+      url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function(message) {
+    $.ajax({
+      url: undefined,
+      type: 'GET',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message received');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to receive message', data);
+      }
+    });
+  },
+  clearMessages: function() {
+    $('#chats').empty();
+  }
+};
